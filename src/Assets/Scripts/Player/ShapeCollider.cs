e086570a329b5b3b1e2a2ea230fa6107f83eba2c
Brett Blashko <blashko.brett@gmail.com>
2016-10-22 19:38:09 -0700

    initialised project. First attempt at player physics

---
+++ src/Assets/Scripts/Player/ShapeCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeCollider : MonoBehaviour {
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        Debug.Log("onCollisionStay");
+        //mIsGrounded = true;
+        //mCanDoubleJump = true;
+    }
+}
