9e1edae0a039162166bd088c8c764de406998288
Brett Blashko <blashko.brett@gmail.com>
2016-11-22 21:21:15 -0800

    Redid how the ground scrolls. The old way has not been deleted. It is just not used currently.

---
+++ src/Assets/Scripts/LineScripts/Ground/GroundScroller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GroundScroller : MonoBehaviour {
+
+    public GameObject[] GroundSections = new GameObject[2];
+    public Vector3 Velocity;
+
+    //Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject section in GroundSections)
+        {
+            section.transform.position += Velocity * Time.deltaTime;
+            if (section.transform.position.x < (-9.025 * 2))
+            {
+                section.transform.position += new Vector3(36.0f, 0.0f, 0.0f);
+            }
+        }
+    }
+}
